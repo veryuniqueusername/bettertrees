852eb331cc449deccdd2617d21ab709039fa459e
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2022-01-10 19:18:11 +0100

    aa

--- src/main/java/io/github/veryuniqueusername/bettertrees/tree/BetterCocoaBeansTreeDecorator.java
+++ src/main/java/io/github/veryuniqueusername/bettertrees/tree/BetterCocoaBeansTreeDecorator.java
@@ -15,36 +15,36 @@ import java.util.List;
 import java.util.Random;
 import java.util.function.BiConsumer;
 
-public class BetterCocoaBeansTreeDecorator extends TreeDecorator {
-//	public static final BetterCocoaBeansTreeDecorator INSTANCE = new BetterCocoaBeansTreeDecorator(0.2f);
-	public static final Codec<BetterCocoaBeansTreeDecorator> CODEC = (Codec.floatRange(0.0f, 1.0f).fieldOf("probability")).xmap(BetterCocoaBeansTreeDecorator::new, decorator -> decorator.probability).codec();
-//	public static final Codec<BetterCocoaBeansTreeDecorator> CODEC = Codec.unit(() -> INSTANCE);
-	private final float probability;
-
-	public BetterCocoaBeansTreeDecorator(float probability) {
-		this.probability = probability;
-	}
-
-	@Override
-	protected TreeDecoratorType<?> getType() {
-		return BetterTreesConfiguredFeatures.BETTER_COCOA_BEANS_DECORATOR;
-	}
-
-	@Override
-	public void generate(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, List<BlockPos> logPositions, List<BlockPos> leavesPositions) {
-		if (random.nextFloat() >= this.probability) {
-			return;
-		}
-		int i = logPositions.get(0).getY();
-		logPositions.stream().filter(pos -> pos.getY() - i <= 2).forEach(pos -> {
-			for (Direction direction: Direction.Type.HORIZONTAL) {
-				Direction direction2;
-				BlockPos blockPos;
-				if (!(random.nextFloat() <= 0.25f) || !Feature.isAir(world, blockPos = pos.add((direction2 = direction.getOpposite()).getOffsetX(), 0, direction2.getOffsetZ())))
-					continue;
-				replacer.accept(blockPos, Blocks.COCOA.getDefaultState().with(CocoaBlock.AGE, random.nextInt(3)).with(CocoaBlock.FACING, direction));
-			}
-		});
-	}
-}
-
+//public class BetterCocoaBeansTreeDecorator extends TreeDecorator {
+////	public static final BetterCocoaBeansTreeDecorator INSTANCE = new BetterCocoaBeansTreeDecorator(0.2f);
+////	public static final Codec<BetterCocoaBeansTreeDecorator> CODEC = (Codec.floatRange(0.0f, 1.0f).fieldOf("probability")).xmap(BetterCocoaBeansTreeDecorator::new, decorator -> decorator.probability).codec();
+////	public static final Codec<BetterCocoaBeansTreeDecorator> CODEC = Codec.unit(() -> INSTANCE);
+//	private final float probability;
+//
+//	public BetterCocoaBeansTreeDecorator(float probability) {
+//		this.probability = probability;
+//	}
+//
+////	@Override
+////	protected TreeDecoratorType<?> getType() {
+////		return BetterTreesConfiguredFeatures.BETTER_COCOA_BEANS_DECORATOR;
+////	}
+//
+//	@Override
+//	public void generate(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, List<BlockPos> logPositions, List<BlockPos> leavesPositions) {
+//		if (random.nextFloat() >= this.probability) {
+//			return;
+//		}
+//		int i = logPositions.get(0).getY();
+//		logPositions.stream().filter(pos -> pos.getY() - i <= 2).forEach(pos -> {
+//			for (Direction direction: Direction.Type.HORIZONTAL) {
+//				Direction direction2;
+//				BlockPos blockPos;
+//				if (!(random.nextFloat() <= 0.25f) || !Feature.isAir(world, blockPos = pos.add((direction2 = direction.getOpposite()).getOffsetX(), 0, direction2.getOffsetZ())))
+//					continue;
+//				replacer.accept(blockPos, Blocks.COCOA.getDefaultState().with(CocoaBlock.AGE, random.nextInt(3)).with(CocoaBlock.FACING, direction));
+//			}
+//		});
+//	}
+//}
+//
